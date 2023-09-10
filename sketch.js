475ec38877f0d0c41ffc69e241207e4d05c73041
Petra <72631662+Luis-Cwk@users.noreply.github.com>
2023-09-10 23:10:12 +0000

    ok

--- sketch.js
+++ sketch.js
@@ -1,104 +1,89 @@
-// "Caos armónico" es coreografía generativa que crea cuerpos representados por líneas vibrantes y coloridas, moviéndose en un espacio abstracto al compás de ritmos sincopados y frenéticos generados por algoritmos, ML e intervención humana. Permitete contemplar el cardumen! Estos cuerpos danzan de manera impredecible, influenciados por un factor de ruido aleatorio que les confiere un movimiento caótico y armónico al mismo tiempo. La obra es una exploración de la belleza encontrada en la interacción de lo aleatorio y lo armónico, creando una experiencia sensorialmente estimulante y evocativa. Los colores, vibrantes y variados, se fusionan y se separan en un constante juego de mezcla y separación, creando un espectáculo visualmente cautivador. "Caos armónico" es una danza que celebra la imprevisibilidad y la armonía en la complejidad del universo. // 
+// Como esa noche de discoteca, cuando se baila, se goza, hay brillos, colores, cierro los ojos y solo vibro, pero tambien tengo en la mente el 7,8... //
+
+// Esta pieza juega con el algoritmo, baila con él, lo ejecuta al ritmo del ordenador, dependiendo de tu maquina, es tu rendimiento, tu performance, en el mío dura como 20 seg en renderizarse, 56 es el frame de inicio para que aparezca la luz, en el 78 termina. Baila cuantas veces quieras, siempre sentiras difente. Petra 13/11/22 //
 
 var colores = [
-  "red",
-  "orange",
   "blue",
-  "indigo",
+  "brown",
   "purple",
   "pink",
-  "yellow", 
-  "green",
-  "cyan",
-  "brown",
+  "#F7A4A4",
+  "#FEBE8C", 
+  "#B6E2A1",
+  "#7743DB",
 ];
 var x = [];
 var y = [];
 var sz = [];
+var col = [];
 var seed = fxrand;
-var noiseScale = 0.2;
-var velocidad = []; 
-let circleX = 0;
-let circleY = 0;
-let xSpeed = 0;
-let ySpeed = 0;
 
-function genR(min, mx) { let result = 0; if (!mx) { result = fxrand() * (min - 0) + 0; } else { result = fxrand() * (mx - min) + min; } return result; }
+function genR(min, max) { let result = 0; if (!max) { result = fxrand() * (min - 0) + 0; } else { result = fxrand() * (max - min) + min; } return result; }
 
 function setup() {
-  createCanvas(windowWidth - 1, windowHeight - 1);
-  frameRate(30);
-  for (var i = 0; i < 9; i++) {
-    x = append(x, genR(-windowWidth / 2, windowWidth / 2));
-    y = append(y, genR(-windowHeight / 2, windowHeight / 2));
-    sz = append(sz, floor(genR(100)));
-    velocidad = append(velocidad, genR(3));
-  }
+  
+  genR(seed);
+  createCanvas(windowWidth, windowHeight);
   extraCanvas = createGraphics(windowWidth, windowHeight);
-}
+    frameRate (8);
 
+  
+  for(var i = 0; i<88; i++){
+    x = append(x,genR(width));
+    y = append(y,genR(height));
+    sz = append(sz,genR(0,150));
+  }
+}
 
 function draw() {
-  background(255);
-  image(extraCanvas, 0, 0, windowWidth, windowHeight);
   
-  for (var i = 0; i < x.length; i++) {
+  background(genR(255), genR(8, 255), genR(8, 255), genR(3));
+  image(extraCanvas, 0,0);
 
+  for(var i = 0; i<x.length; i++){
+    
     extraCanvas.push();
-    var steps = sz[i];
+    extraCanvas.translate(x[i], y[i]);
     
-    for (var j = 1; j < y.length; j+=steps) {
-      
-      let noiseVal = noise(x[i] + noiseScale, y[j] * steps);
-      
-      x[noiseVal - 1] += genR(-sz[i], sz[i]);
-      y[noiseVal - 1] += genR(-sz[i], sz[i]);
-
-      x[j - 1] = constrain(x[j], 0, windowWidth);
-      y[j - 1] = constrain(y[j], 0, windowHeight);
-      
-     
+    var steps = sz[i]/8;
     
-      var x1 = x[i] + angle;
-      var y1 = sz[i] - angle;
-      var x2 = y[i] * angle;
-      var y2 = velocidad[i] + angle;
+    for (var j = 0; j < sz[i]; j += steps){
+      
+      let ang = frameCount * genR(8); 
+      
+      extraCanvas.rotate(ang);
+      
+      extraCanvas.strokeWeight(genR(.2));
+      extraCanvas.stroke(genR(255), genR(255), genR(255),genR(8));
 
-      extraCanvas.stroke(colores[i]);        
-      extraCanvas.fill(genR(colores[i]));
-              
-      var tx = width / 2 + 50 * sin(x[i] + frameCount * 0.02);
-      var ty = height / 2 + 51 * cos(y[i] + frameCount * 0.03); 
-        
-      extraCanvas.translate(tx, ty);
-        
-      extraCanvas.rotate(fract(angle,frameCount * 0.008,tx));
-              
-      circleX += xSpeed;
-        if (circleX < x[i] || circleX > angle) {
-          xSpeed *= -1;
-        }
+      extraCanvas.rectMode(CENTER);
 
-      circleY += ySpeed;
-        if (circleY < y[i] || circleY > angle) {
-          ySpeed *= -1;
-        }
+      extraCanvas.rect(8*j, 8/j, ang/.2, 8/steps, steps/8 , 8/j);
 
-      let val = circleX * noiseVal / tx;
-
-      extraCanvas.line(x1 || val, y1, x[j] || x1, y[j] || val, x2, val, x[j-1], y1);
-    }   
+      extraCanvas.fill(genR(0, 250), genR(0, 250), genR (250), genR(3)); 
     
-    extraCanvas.pop();
+    }
+    extraCanvas.translate(width, height);
+    extraCanvas.translate(p5.Vector.fromAngle(millis() / seed, seed)); 
+    
+    extraCanvas.pop();  
   }
-  
-  if (frameCount == 3000) {
-    noLoop();
-  }  
+  if (isFxpreview == true) {
+    fxpreview();
+  }
+  if (frameCount == 56) {
+    extraCanvas.blendMode(ADD);
+  }
+  console.log(frameCount);
+  if (frameCount == 88) {
+		noLoop();
+  }
 }
 
-if (isFxpreview == true) {
-  fxpreview();
+function keyPressed() {
+    if (key === 's') {
+        saveGif('mySketch', 5);
+    }
 }
 
 // DATA // 
